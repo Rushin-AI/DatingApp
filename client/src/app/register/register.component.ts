5ec4230ca0e06d7781fac3d37eb3959513b03026
Rushin-AI <rmakwana@aakashinformaics.com>
2022-04-08 18:13:48 +0530

    End of Client login and regsister

---
+++ client/src/app/register/register.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
+import { AccountService } from '../_services/account.service';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent implements OnInit {
+
+@Output() cancelRegister = new EventEmitter(); 
+registerMode: any ={}; 
+  model: any ={};
+  constructor(private accountService: AccountService) { }
+
+  ngOnInit(): void {
+  }
+  register(){
+  this.accountService.register(this.model).subscribe(response =>{
+    console.log(response);
+    this.cancel();
+  }, error=>
+  console.log(error)) 
+      console.log(this.model);
+}
+cancel(){
+this.cancelRegister.emit(false);
+}
+}
